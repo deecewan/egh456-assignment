c0f92115df750a6cbe809fa1c6f1e064ca37e8bc
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-05-03 23:17:45 +1000

    re-enable runtime tracking

--- ui/main.c
+++ ui/main.c
@@ -7,7 +7,7 @@
 #include <driverlib/rom_map.h>
 #include <inc/hw_memmap.h>
 #include <ti/sysbios/knl/Task.h>
-//#include <ti/sysbios/knl/Clock.h>
+#include <ti/sysbios/knl/Clock.h>
 #include "drivers/kentec320x240x16_ssd2119.h"
 #include "drivers/frame.h"
 #include "drivers/touch.h"
@@ -21,33 +21,14 @@
 
 Char taskRedrawLoopStack[TASKSTACKSIZE];
 Task_Struct taskRedrawLoopStruct;
-//Clock_Struct clockRuntimeTrackerStruct;
+Clock_Struct clockRuntimeTrackerStruct;
 
 MOTOR_STATE last_known_state;
 
-Void taskRedrawLoop(UArg arg0, UArg arg1) {
-  while (1) {
-      MOTOR_POWER power = get_motor_power();
-      switch (power) {
-          case ON:
-            ROM_GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_5, 0x0);
-            ROM_GPIOPinWrite(GPIO_PORTQ_BASE, GPIO_PIN_7, GPIO_PIN_7);
-            break;
-          case OFF:
-            ROM_GPIOPinWrite(GPIO_PORTQ_BASE, GPIO_PIN_7, 0x0);
-            ROM_GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_5, GPIO_PIN_5);
-            break;
-        }
-      MOTOR_STATE state = get_motor_state();
-      if (state != last_known_state) {
-          last_known_state = state;
-          tabs_onStateChange();
-      }
-    // process anything pending
-    WidgetMessageQueueProcess();
-  }
-}
-
+// this task runs when the system is idle
+// at the lowest priority to redraw the screen
+// contents. It really isn't that important when
+// (within reason), the screen is redrawn
 Void idleTask() {
     MOTOR_POWER power = get_motor_power();
     switch (power) {
@@ -69,11 +50,11 @@ Void idleTask() {
     WidgetMessageQueueProcess();
 }
 
-//Void clockRuntimeTracker(UArg arg) {
-//    increment_run_time();
-//
-//    update_runtime_display();
-//}
+Void clockRuntimeTracker(UArg arg) {
+    increment_run_time();
+
+    update_runtime_display();
+}
 
 void ui_setup(uint32_t sysclock) {
   // Init the display driver
@@ -95,13 +76,17 @@ void ui_setup(uint32_t sysclock) {
   // perform the first paint of the widgets
   WidgetPaint(WIDGET_ROOT);
 
-//  // we kick off a task in here to handle drawing
-//  Task_Params taskParams;
-//  Task_Params_init(&taskParams);
-//  taskParams.stackSize = TASKSTACKSIZE;
-//  taskParams.stack = &taskRedrawLoopStack;
-//  taskParams.priority = 10;
-//  Task_construct(&taskRedrawLoopStruct, (Task_FuncPtr)taskRedrawLoop, &taskParams, NULL);
+  // set up clock to track run time
+  Clock_Params clkParams;
+  Clock_Params_init(&clkParams);
+  clkParams.startFlag = TRUE;
+  clkParams.period = 1000;
+  Clock_construct(
+      &clockRuntimeTrackerStruct,
+      (Clock_FuncPtr)clockRuntimeTracker,
+      1,
+      &clkParams
+  );
 }
 
 void make_background_color(uint32_t color) {
