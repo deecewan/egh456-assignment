26c08fe0b87d52ebd5e204d5ab5a01505a3d4f64
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-05-05 17:15:58 +1000

    start building out stats graph screen

--- ui/tabs/stats.c
+++ ui/tabs/stats.c
@@ -5,10 +5,71 @@
 #include <grlib/canvas.h>
 #include <grlib/pushbutton.h>
 #include "drivers/kentec320x240x16_ssd2119.h"
+#include "../tabs.h"
+#include "stats.h"
+
+static VISIBILITY visibility = LINE_MOTOR_SPEED | LINE_CURRENT | LINE_TEMP;
+
+void toggle_vis_motor_speed();
+void toggle_vis_current();
+void toggle_vis_temp();
+
+void toggle_visibility(VISIBILITY vis) {
+    visibility ^= vis;
+}
+
+RectangularButton(legendMotorSpeed, 0, 0, 0, &g_sKentec320x240x16_SSD2119,
+  10, 240 - 24 - 8 - 30, 100, 16,
+  PB_STYLE_TEXT | PB_STYLE_FILL | PB_STYLE_RELEASE_NOTIFY, ClrChartreuse, ClrChartreuse, 0, ClrBlack,
+  g_psFontCmss16, "Motor Speed", 0, 0, 0, 0, toggle_vis_motor_speed);
+
+RectangularButton(legendCurrent, 0, 0, 0, &g_sKentec320x240x16_SSD2119,
+  110, 240 - 24 - 8 - 30, 100, 16,
+  PB_STYLE_TEXT | PB_STYLE_FILL | PB_STYLE_RELEASE_NOTIFY, ClrCornflowerBlue, ClrCornflowerBlue, 0, ClrBlack,
+  g_psFontCmss16, "Current", 0, 0, 0, 0, toggle_vis_current);
+
+RectangularButton(legendTemp, 0, 0, 0, &g_sKentec320x240x16_SSD2119,
+  210, 240 - 24 - 8 - 30, 100, 16,
+  PB_STYLE_TEXT | PB_STYLE_FILL | PB_STYLE_RELEASE_NOTIFY, ClrDeepPink, ClrDeepPink, 0, ClrBlack,
+  g_psFontCmss16, "Temperature", 0, 0, 0, 0, toggle_vis_temp);
+
+void paint_legend_item(VISIBILITY filter, tPushButtonWidget *button, uint32_t color) {
+    uint32_t fill, text;
+    if (visibility & filter) {
+        fill = color;
+        text = ClrBlack;
+    } else {
+        fill = ClrBlack;
+        text = color;
+    }
+
+    PushButtonFillColorSet(button, fill);
+    PushButtonTextColorSet(button, text);
+    WidgetPaint((tWidget *)button);
+}
+
+void toggle_vis_motor_speed() {
+    toggle_visibility(LINE_MOTOR_SPEED);
+    paint_legend_item(LINE_MOTOR_SPEED, &legendMotorSpeed, ClrChartreuse);
+}
+
+void toggle_vis_current() {
+    toggle_visibility(LINE_CURRENT);
+    paint_legend_item(LINE_CURRENT, &legendCurrent, ClrCornflowerBlue);
+}
+
+void toggle_vis_temp() {
+    toggle_visibility(LINE_TEMP);
+    paint_legend_item(LINE_TEMP, &legendTemp, ClrDeepPink);
+}
 
 void paint_stats(tWidget *psWidget, tContext *psContext) {
-    GrContextFontSet(psContext, g_psFontCm16);
-    GrContextForegroundSet(psContext, ClrSilver);
-    GrStringDraw(psContext, "Stats Panel", -1,
-                    10, 30, 0);
+    GrContextForegroundSet(psContext, ClrWhite);
+    GrLineDraw(psContext, 10, 28, 10, 173);
+    GrLineDraw(psContext, 10, 173, 309, 173);
+
+    // Draw legend indicators
+    WidgetAdd(psWidget, (tWidget *)&legendMotorSpeed);
+    WidgetAdd(psWidget, (tWidget *)&legendCurrent);
+    WidgetAdd(psWidget, (tWidget *)&legendTemp);
 }
--- ui/tabs/stats.h
+++ ui/tabs/stats.h
@@ -3,5 +3,11 @@
 #include <grlib/grlib.h>
 #include <grlib/widget.h>
 
+typedef enum VISIBILITY {
+    LINE_MOTOR_SPEED = 0b1,
+    LINE_CURRENT = 0b10,
+    LINE_TEMP = 0b100,
+} VISIBILITY;
+
 void paint_stats(tWidget *psWidget, tContext *psContext);
 #endif // UI_TABS_STATS_H
