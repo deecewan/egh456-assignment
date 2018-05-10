a589587eb89c7b82258abda4e8b66596aed04a0c
Dhiraj Kumar <dhiraj.kumar@connect.qut.edu.au>
2018-05-10 10:55:28 +1000

    sends out a PWM wave, still configuring control scheme given in supporting document.

--- motor/speed.c
+++ motor/speed.c
@@ -3,6 +3,8 @@
 #include <driverlib/sysctl.h>
 #include <driverlib/pin_map.h>
 #include <driverlib/gpio.h>
+#include <driverlib/timer.h>
+#include <driverlib/pwm.h>
 #include <inc/hw_memmap.h>
 
 #define NUM_STATES 6
@@ -17,6 +19,9 @@
 
 const uint8_t HALL_SENSOR_STATES[NUM_STATES] = {1, 101, 100, 110, 10, 11};
 static uint8_t checkpoint_state; // can only be changed through publicly available functions
+static uint16_t counter = 1000;
+static double duty_cycle = 0.25;
+
 
 /*
  * Function Prototypes.
@@ -64,7 +69,7 @@ double GetMotorSpeed() {
     }
 
     difference = checkpoint_state - current_state;
-    if (difference <= 0) {
+    if (difference < 0) {
         difference += NUM_STATES;
     }
 
@@ -74,25 +79,93 @@ double GetMotorSpeed() {
 }
 
 void StartMotor() {
+    // Initialise timer hardware for PWM wave
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER2);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER3);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOM);
+    GPIOPinConfigure(GPIO_PM2_T3CCP0);
+    GPIOPinTypeTimer(GPIO_PORTM_BASE, GPIO_PIN_2);
+
+    // Configure timer and by extension, PWM parameters
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM);
+    TimerLoadSet(TIMER0_BASE, TIMER_A, 20000);
+    TimerMatchSet(TIMER0_BASE, TIMER_A, counter); // PWM
+    TimerEnable(TIMER0_BASE, TIMER_A);
+
+    while(1) {
+        counter++;
+        TimerMatchSet(TIMER0_BASE, TIMER_A, counter); // PWM
+
+        if (counter > 10000) {
+            counter = 0;
+        }
+        SysCtlDelay(30000);
+    }
+    /*
+    TimerConfigure(TIMER2_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM | TIMER_CFG_B_PWM);
+    TimerLoadSet(TIMER2_BASE, TIMER_BOTH, SysCtlClockGet());
+    TimerMatchSet(TIMER2_BASE, TIMER_BOTH, 0); // PWM
+    TimerConfigure(TIMER3_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM | TIMER_CFG_B_PWM);
+    TimerLoadSet(TIMER3_BASE, TIMER_BOTH, SysCtlClockGet());
+    TimerMatchSet(TIMER3_BASE, TIMER_BOTH, 0); // PWM
+    TimerEnable(TIMER2_BASE, TIMER_BOTH);
+    TimerEnable(TIMER3_BASE, TIMER_BOTH);
+    */
+/*
     // Initiate connection with motor's half bridges and fault sensor
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOM);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOL);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
-    // Initialise PWM hardware
-    SysCtlPWMClockSet(SYSCTL_PWMDIV_1); // Set the PWM clock to the system clock.
+    // Configure PWM pins
     GPIOPinConfigure(GPIO_PM0_T2CCP0);
     GPIOPinConfigure(GPIO_PM1_T2CCP1);
     GPIOPinConfigure(GPIO_PM2_T3CCP0);
     GPIOPinConfigure(GPIO_PA7_T3CCP1);
     GPIOPinConfigure(GPIO_PL4_T0CCP0);
     GPIOPinConfigure(GPIO_PL5_T0CCP1);
-    GPIOPinTypePWM(GPIO_PORTM_BASE, GPIO_PIN_0);
-    GPIOPinTypePWM(GPIO_PORTM_BASE, GPIO_PIN_1);
-    GPIOPinTypePWM(GPIO_PORTM_BASE, GPIO_PIN_2);
-    GPIOPinTypePWM(GPIO_PORTA_BASE, GPIO_PIN_7);
-    GPIOPinTypePWM(GPIO_PORTL_BASE, GPIO_PIN_4);
-    GPIOPinTypePWM(GPIO_PORTL_BASE, GPIO_PIN_5);
+    GPIOPinTypeTimer(GPIO_PORTM_BASE, GPIO_PIN_0);
+    GPIOPinTypeTimer(GPIO_PORTM_BASE, GPIO_PIN_1);
+    GPIOPinTypeTimer(GPIO_PORTM_BASE, GPIO_PIN_2);
+    GPIOPinTypeTimer(GPIO_PORTA_BASE, GPIO_PIN_7);
+    GPIOPinTypeTimer(GPIO_PORTL_BASE, GPIO_PIN_4);
+    GPIOPinTypeTimer(GPIO_PORTL_BASE, GPIO_PIN_5);
+
+    // Configure timer and by extension, PWM parameters
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM | TIMER_CFG_B_PWM);
+    TimerLoadSet(TIMER0_BASE, TIMER_BOTH, SysCtlClockGet());
+    TimerMatchSet(TIMER0_BASE, TIMER_BOTH, (uint32_t)(duty_cycle * SysCtlClockGet())); // PWM
+    TimerConfigure(TIMER2_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM | TIMER_CFG_B_PWM);
+    TimerLoadSet(TIMER2_BASE, TIMER_BOTH, SysCtlClockGet());
+    TimerMatchSet(TIMER2_BASE, TIMER_BOTH, 0); // PWM
+    TimerConfigure(TIMER3_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM | TIMER_CFG_B_PWM);
+    TimerLoadSet(TIMER3_BASE, TIMER_BOTH, SysCtlClockGet());
+    TimerMatchSet(TIMER3_BASE, TIMER_BOTH, 0); // PWM
+    TimerEnable(TIMER0_BASE, TIMER_BOTH);
+    TimerEnable(TIMER2_BASE, TIMER_BOTH);
+    TimerEnable(TIMER3_BASE, TIMER_BOTH);
+
+    // Configure PWM parameters
+    PWMGenConfigure(PWM0_BASE, PWM_GEN_0, PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
+    //
+    // Set the PWM period to 250Hz.  To calculate the appropriate parameter
+    // use the following equation: N = (1 / f) * SysClk.  Where N is the
+    // function parameter, f is the desired frequency, and SysClk is the
+    // system clock frequency.
+    // In this case you get: (1 / 250Hz) * 16MHz = 64000 cycles.  Note that
+    // the maximum period you can set is 2^16 - 1.
+    // TODO: modify this calculation to use the clock frequency that you are
+    // using.
+    //
+    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_0, 64000);
+    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, PWMGenPeriodGet(PWM0_BASE, PWM_GEN_0) / 4);
+
+    // Enable PWM signal
+    PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT | PWM_OUT_0_BIT, true);
+    PWMGenEnable(PWM0_BASE, PWM_GEN_0);
+    */
 }
 
 void SetMotorSpeed() {
